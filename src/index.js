57da358f3863ce2fa53179b29a339595edcb8773
Dmitry Golovanov <dmgolovanov@gmail.com>
2016-11-10 15:08:18 +0300

    task 2B

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import XRegExp from 'xregexp';
 
 const app = express();
 
@@ -16,6 +17,34 @@ app.get('/task2a', (req, res) => {
   const sum = a + b;
   console.log(`a=${a}, b=${b}, sum=${sum}`);
   res.send(sum.toString());
+});
+
+const regex = new XRegExp('^\\s*([\\p{L}\']+)\\s*([\\p{L}\']*)\\s*([\\p{L}\']*)$');
+
+function fSurname(sname) {
+  return `${sname.substr(0, 1).toUpperCase()}${sname.substr(1).toLowerCase()}`;
+}
+
+function fInitials(name) {
+  return `${name.substr(0, 1).toUpperCase()}.`;
+}
+
+function convertFullname(fullname) {
+  const m = regex.exec(fullname);
+  console.log(m);
+  if (m) {
+    if (!m[2]) return fSurname(m[1]);
+    if (!m[3]) return `${fSurname(m[2])} ${fInitials(m[1])}`;
+    return `${fSurname(m[3])} ${fInitials(m[1])} ${fInitials(m[2])}`;
+  }
+  return 'Invalid fullname';
+}
+
+app.get('/task2b', (req, res) => {
+  console.log(req.query);
+  const name = convertFullname(req.query.fullname);
+  console.log(`name = ${name}`);
+  res.send(name);
 });
 
 app.listen(3000, () => console.log('app listening on port 3000'));
